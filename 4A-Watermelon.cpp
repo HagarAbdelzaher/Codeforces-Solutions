43a66d682add9b0753ac06265539a5f2da3324c9
HagarAbdelzaher <50349835+HagarAbdelzaher@users.noreply.github.com>
2021-01-30 03:23:30 +0200

    Create 4A-Watermelon.cpp

---
+++ 4A-Watermelon.cpp
@@ -0,0 +1,37 @@
+#define  FOR(i,n) for(int i=0;i<n;i++)
+#define rFOR(i,x,n) for(int i=x;i>n;i--)
+#define SZ(x) (int)x.size()
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+typedef long long ll;
+//const int oo=(1LL<<30);
+//const ll  OO=(1LL<<62);
+//const int MaxSize=1e2+2;
+//const double PI=3.141592653589;
+#include <stdio.h>
+#include <iostream>
+#include<bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+void run() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr), cout.tie(nullptr);
+}
+int arr[50];
+vector<int> even , odd;
+int main()
+{
+    run();
+
+        int n;
+       cin>>n;
+       if( n%2!=0 || n < 3 )
+        cout<<"NO"<<endl;
+       else
+        cout<<"YES"<<endl;
+
+
+}
+ 
