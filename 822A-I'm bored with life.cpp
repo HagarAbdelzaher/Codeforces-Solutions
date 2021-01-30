136b9f264a6889dd16f313a8632026f7216c9006
HagarAbdelzaher <50349835+HagarAbdelzaher@users.noreply.github.com>
2021-01-30 20:30:24 +0200

    Create 822A-I'm bored with life.cpp

---
+++ 822A-I'm bored with life.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <bits/stdtr1c++.h>
+using namespace std;
+#define ll  long long
+#define ld long double
+#define pb push_back
+#define INF 0x3f3f3f3f
+#define vi vector<int>
+#define vl vector<long long>
+#define print(el) cout<<(el)<<endl;
+const int N = int(1e5 + 3);
+const long double PI = 3.1415926536;
+
+int main()
+{
+    ll n , m , ans=1;
+    cin>>n>>m;
+    int mn = min(n,m);
+    for(int i=1 ; i<=mn ; i++)
+        ans*=i;
+
+    cout<<ans;
+}
