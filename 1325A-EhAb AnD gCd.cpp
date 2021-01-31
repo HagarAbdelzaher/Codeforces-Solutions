6178493b84b267de150dab51cb636046fa1130a1
HagarAbdelzaher <hagarr.abdelzaher@gmail.com>
2021-01-31 21:31:54 +0200

    Create 1325A-EhAb AnD gCd.cpp

---
+++ 1325A-EhAb AnD gCd.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <bits/stdtr1c++.h>
+using namespace std;
+#define ll  long long
+#define ld long double
+#define pb push_back
+#define INF 0x3f3f3f3f
+#define vi vector<int>
+#define vl vector<long long>
+#define print(el) cout<<(el)<<endl;
+
+const int N = int(1e5 + 3);
+const long double PI = 3.1415926536;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while (t--)
+    {
+    int a;
+    cin>>a;
+    cout<< 1 << " " << a-1 <<endl;
+    }
+}
