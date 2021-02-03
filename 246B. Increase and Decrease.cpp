5216c27645d96a62b6e1789095f81e95d79bdbe8
HagarAbdelzaher <hagarr.abdelzaher@gmail.com>
2021-02-03 22:39:16 +0200

    Create 246B. Increase and Decrease.cpp

---
+++ 246B. Increase and Decrease.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <bits/stdtr1c++.h>
+using namespace std;
+#define ll  long long
+#define ld long double
+#define pb push_back
+#define INF 0x3f3f3f3f
+//#define for(i,n) for(int i = 0 ; i < n ; i++)
+#define vi vector<int>
+#define vl vector<long long>
+#define print(el) cout<<(el)<<endl;
+//    freopen ("input.txt", "r", stdin);
+//    freopen ("output.txt", "w", stdout);
+
+const int N = int(1e5 + 3);
+const long double PI = 3.1415926536;
+ll a[int(1e6)];
+ll b[int(1e6)];
+//vector<ll> vis;
+
+int main()
+{
+ll n , sum=0;
+cin>>n;
+for(int i=0 ; i<n ; i++)
+{
+    cin>>a[i];
+    sum+=a[i];
+}
+
+cout<< ((sum%n==0) ? n : n-1 )<<endl;
+}
