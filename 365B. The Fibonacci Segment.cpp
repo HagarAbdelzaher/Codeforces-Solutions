ec934523dcc56d6a01a13036ca2f631386ad87c9
HagarAbdelzaher <hagarr.abdelzaher@gmail.com>
2021-02-03 22:43:33 +0200

    Create 365B. The Fibonacci Segment.cpp

---
+++ 365B. The Fibonacci Segment.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <bits/stdtr1c++.h>
+using namespace std;
+#define ll  long long
+#define ld long double
+#define pb push_back
+#define INF 0x3f3f3f3f
+//#define for(i,n) for(int i = 0 ; i < n ; i++)
+#define vi vector<int>
+#define vl vector<long long>
+#define print(el) cout<<(el)<<endl;
+//    freopen ("input.txt", "r", stdin);
+//    freopen ("output.txt", "w", stdout);
+
+const int N = int(1e5 + 3);
+const long double PI = 3.1415926536;
+ll a[int(1e6)];
+ll b[int(1e6)];
+//vector<ll> vis;
+ll fun(vl v , ll n )
+{
+    if (n <= 2)
+        return n;
+    int len = 2 , mx = INT_MIN;
+    for(int i = 2 ; i < n ; i++)
+    {
+        if(v[i] == (v[i-1]+v[i-2]))
+            len++;
+        else
+            len = 2;
+        mx = max(mx,len);
+    }
+    return mx;
+}
+int main()
+{
+ll n , x;
+cin>>n;
+vl v;
+for(ll i=0 ; i<n ; i++)
+{
+    cin>>x;
+    v.pb(x);
+}
+
+cout<<fun(v,n);
+
+}
