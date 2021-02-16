aaed733c1ff18aad79052dbda7c118a5f031c1a4
HagarAbdelzaher <hagarr.abdelzaher@gmail.com>
2021-02-16 05:17:21 +0200

    Create 688B. Lovely Palindromes.cpp

---
+++ 688B. Lovely Palindromes.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <bits/stdtr1c++.h>
+#include<algorithm>
+#define ll  long long
+using namespace std;
+const int N = int(1e5 + 3);
+const long double PI = 3.1415926536;
+//freopen("input.txt", "r", stdin);
+// freopen("output.txt", "w", stdout);
+int a[int(1e6)];
+int sum[int(1e6)];
+
+int main()
+{
+    string s;
+    cin>>s;
+    cout<<s;
+    reverse(s.begin() , s.end());
+    cout<<s;
+
+}
+
+ 
