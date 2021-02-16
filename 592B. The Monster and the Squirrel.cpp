32339cb4d848b564ff6b492c994362e45e5957e0
HagarAbdelzaher <hagarr.abdelzaher@gmail.com>
2021-02-16 06:17:06 +0200

    Create 592B. The Monster and the Squirrel.cpp

---
+++ 592B. The Monster and the Squirrel.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <bits/stdtr1c++.h>
+#include<algorithm>
+#define ll  long long
+using namespace std;
+const int N = int(1e5 + 3);
+const long double PI = 3.1415926536;
+//freopen("input.txt", "r", stdin);
+// freopen("output.txt", "w", stdout);
+int arr[N];
+int main()
+{
+   ll n;
+   cin>>n;
+   cout<<(n-2)*(n-2)<<endl;
+
+}
+ 
